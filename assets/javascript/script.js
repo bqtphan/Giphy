a942803f46cdbfd2a5094f8be6afbe83ea50da01
bqtphan <bqtphan@utexas.edu>
2018-06-09 17:35:42 -0500

    almost done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,27 +5,29 @@ function generateButtons() {
     for (var i = 0; i < topics.length; i++) {
         var addButton = $("<button>");
         addButton.addClass("btn btn-outline-dark btn-lg");
-        addButton.attr("data-name", topics[i]);
+        addButton.attr("data-value", topics[i]);
         addButton.text(topics[i]);
         $("#tags").append(addButton);
     }
 }
+generateButtons();
 
 $("#submit").on("click", function (event) {
     event.preventDefault();
     var newButton = $("#yourInput").val().trim();
     var addInputButton = $("<button>");
-    addInputButton.attr("data-name", newButton);
     addInputButton.addClass("btn btn-outline-dark btn-lg");
+    addInputButton.attr("data-value", newButton);
     addInputButton.text(newButton);
     $("#tags").append(addInputButton);
 })
 
-$("button").on("click", function () {
-    var buttonClicked = $(this).attr("data-name");
-    console.log(buttonClicked);
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonClicked + "&api_key=dc6zaTOxFJmzC&limit=10";
+$(".btn").on("click", function () {
+    event.preventDefault();
+    // $("#gifs").remove();
+    var searchItem = $(this).attr("data-value");
+    console.log(searchItem);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchItem + "&api_key=rBjSvGhSZWu1qGzVb1HfMBfWlmHKSAVP&limit=10";
 
     $.ajax({
             url: queryURL,
@@ -53,5 +55,3 @@ $("button").on("click", function () {
         });
 });
 
-
-generateButtons();
